9e8bf7b4ae24c97cda0e437e235cbf958b1807a8
Trufelli <thiagotrufelli@gmail.com>
2018-04-10 15:20:53 -0300

    Ajustes no Script, adicao de instalacao pelo Chocolatey

--- Roteiro_Nova_Instalacao.bat
+++ Roteiro_Nova_Instalacao.bat
@@ -5,15 +5,15 @@
 :: # Script para "Automatizacao Basica de formatação/instalação de novos equipamentos (Windows)"
 :: # Criado por: Thiago de Mello S. G. Trufelli - thiagotrufelli@meuspedidos.com.br
 :: # Data de Criação: 16/06/2016
-:: # Data de ultima Alteração: 26/10/2017 - Removido a instalação do Google Drive e adicionado a instalação do Google Drive File Stream
+:: # Data de ultima Alteração: 10/04/2018 - Realizado ajustes no script, adicionado instalações pelo Chocolatey
 :: #
 :: #####
 
+:: Ler senhas do arquivo apropiado
+set /p adm_Pass=<secret.txt
+
 :: Define Charset para UTF-8
 chcp 65001
-
-:: Limpa o Win 10
-::PowerShell.exe -noprofile -executionpolicy bypass -file %UserProfile%\Desktop\Roteiro_Nova_Instalacao\Windows10_Decrapifier_version_1.ps1
 
 :: Renomeia o Windows e adiciona ao grupo de trabalho "MEUSPEDIDOS"
 PowerShell.exe -noprofile -executionpolicy bypass -file %UserProfile%\Desktop\Roteiro_Nova_Instalacao\Rename_Windows.ps1
@@ -25,7 +25,7 @@ rmdir C:\Users\Administrador /S /Q
 net user administrador /active:yes
 
 :: Adiciona a senha ao usuário "Administrador" e define que ela nunca expirará
-net user administrador m3us@p3d1d0s /expires:never 
+net user administrador %adm_Pass% /expires:never 
 
 :: Cria diretório para armazenar o Wallpaper
 mkdir c:\Temp\Wallpaper
@@ -33,90 +33,48 @@ mkdir c:\Temp\Wallpaper
 :: Copiar Wallpaper para o diretório
 copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Imagens\wallpaper.bmp" "C:\Temp\Wallpaper\wallpaper.bmp"
 
-:: Define o Wallpaper do MPlayer
-::reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Temp\Wallpaper\wallpaper.bmp /f
-
-:: Defnie o Lockscreen do MPlayer
-::REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization /V LockScreenImage /T REG_SZ /D C:\temp\wallpaper\wallpaper.bmp /F
-
 :: Desabilita o UAC
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
-
-:: Desabilita a Cortana
-::reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0 /f
 
 :: Desabilita o Firewall
 netsh advfirewall set allprofiles state off
 
 :: Desabilitar notificações sobre o Firewall
-netsh firewall set notifications mode = disable profile = all 
+::netsh firewall set notifications mode = disable profile = all 
 
 :: Libera acesso a \\ e Psexec
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
 
-:: Aplca a alteração
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+:: Aplica a alteração
+::RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
 :: Desabilita Windows Defender Security Center e remove do startup
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SecurityHealthService" /v Type /t REG_DWORD /d 3 /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v SecurityHealth /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v WindowsDefender /f
 
-:: Renomeia arquivos originais de imagens do usuário
-::ren "C:\ProgramData\Microsoft\User Account Pictures\guest.bmp" "guest.original.bmp"
-::ren "C:\ProgramData\Microsoft\User Account Pictures\user.bmp" "user.original.bmp"
-::ren "C:\ProgramData\Microsoft\User Account Pictures\user-32.png" "user-32.original.png"
-::ren "C:\ProgramData\Microsoft\User Account Pictures\user-40.png" "user-40.original.png"
-::ren "C:\ProgramData\Microsoft\User Account Pictures\user-48.png" "user-48.original.png"
-::ren "C:\ProgramData\Microsoft\User Account Pictures\user-192.png" "user-192.original.png"
-
-:: Copia os arquivos da MP de imagens do usuário
-::copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Imagens\guest.bmp" "C:\ProgramData\Microsoft\User Account Pictures\guest.bmp"
-::copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Imagens\user.bmp" "C:\ProgramData\Microsoft\User Account Pictures\user.bmp"
-::copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Imagens\user-32.png" "C:\ProgramData\Microsoft\User Account Pictures\user-32.png"
-::copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Imagens\user-40.png" "C:\ProgramData\Microsoft\User Account Pictures\user-40.png"
-::copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Imagens\user-48.png" "C:\ProgramData\Microsoft\User Account Pictures\user-48.png"
-::copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Imagens\user-192.png" "C:\ProgramData\Microsoft\User Account Pictures\user-192.png"
-
-:: Instala o 7zip
-msiexec /i "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\7z1604-x64.msi" /quiet
-
 :: Instala Open Office (Apenas Calc,Writer e Impress)
 msiexec.exe /i "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\OpenOffice 4.1.3 (pt-BR) Installation Files\openoffice413.msi" ALLUSERS=1 /qn /norestart SETUP_USED=1 RebootYesNo=No CREATEDESKTOPLINK=0 ADDLOCAL=ALL REMOVE=gm_o_Quickstart,gm_p_Base,gm_p_Base_Bin,gm_p_Draw,gm_p_Draw_Bin,gm_p_Math,gm_p_Math_Bin
 
-:: Instala o Skype
-%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\SkypeSetup.exe /silent /install
+:: Install Chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 :: Remove atalho do Skype de todos os usuários
 del "C:\Users\Public\Desktop\Skype.lnk"
 
-:: Instala o Chrome
-%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\ChromeStandaloneSetup64.exe /silent /install
-
-:: Instala o Notepad++
-%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\npp.7.5.4.Installer.x64.exe /S
-
-:: Instala o Avast
-%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\avast_business_antivirus_setup_offline.exe /silent
-
-:: Remove o atalho do Avast de todos os usuários
-del "C:\Users\Public\Desktop\Avast Business Security.lnk"
-
-:: Instala Slack
-%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\SlackSetup.exe
-
-:: Instala o Drive ( Substituido pelo Google Drive File Stream )
-::msiexec /i %UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\gsync_enterprise.msi /quiet
-%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\GoogleDriveFSSetup.exe --silent
-
-:: Instala Adobe Reader 
-%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\AdbeRdr11000_pt_BR.exe /msi EULA_ACCEPT=YES /qn
+:: Chocolatey install Programs (Chrome,Notepad++,Slack,Skype
+choco install Chrome -y
+choco install notepadplusplus.install -y
+choco install Slack -y
+choco install google-drive-file-stream -y
+choco install adobereader
+choco install Skype -y
 
 :: Remove atalho do Adobe de todos os usuários
 del "C:\Users\Public\Desktop\Adobe Reader XI.lnk"
 
-:: Chama o instalador do Hosanna
-::%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\Setup_HosannaAiOAgent-4.0.0.4_rc6.exe"
+:: Instala o Avast
+%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\avast_business_antivirus_setup_offline.exe /silent
 
 :: Instala plugin do Chrome Click2Call
 regedit.exe /s %UserProfile%\Desktop\Roteiro_nova_Instalacao\install_click2call.reg
@@ -141,11 +99,6 @@ REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2E
 :: Executa Windows Update
 cscript %UserProfile%\Desktop\Roteiro_Nova_Instalacao\Windows_Update.vbs
 
-:: Remove Apps Nativos do Win10 desnecessários (Twitter, Facebook, Netflix, etc...)
-
-::PowerShell.exe -noprofile -executionpolicy bypass -file %UserProfile%\Desktop\Roteiro_Nova_Instalacao\Remove_Win_10_Apps.ps1
-::PowerShell.exe -noprofile -executionpolicy bypass -file %UserProfile%\Desktop\Roteiro_Nova_Instalacao\Windows10_Decrapifier_version_1.ps1
-
 :: Altera o Wallpaper para da MP
 REG ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /d "C:\temp\Wallpaper\wallpaper.bmp" /f
 
@@ -159,7 +112,5 @@ net localgroup administradores /delete mplayer
 :: Instalar VNC
 %UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\vnc\UltraVNC_1_2_12_X64_Setup.exe /verysilent /loadinf="%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\vnc\vncinstall.inf" /log /no restart
 copy "%UserProfile%\Desktop\Roteiro_Nova_Instalacao\Pacotes\vnc\ultravnc.ini" "%ProgramFiles%\UltraVNC\ultravnc.ini" /Y
-::net stop uvnc_service 
-::net start uvnc_service 
 
 pause
